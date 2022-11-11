1f2583944eae2a25e71af9b818b28335bf4bb370
Leonard Barbosa <leonardobrpereira2015@gmail.com>
2022-11-10 21:34:59 -0300

    adicionando sistema de detalhes e estilizando avisos com o react-toastify

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,13 @@
 import { useEffect, useState} from 'react';
 import api from '../../services/api';
-import "./home.css"
+import { Link } from 'react-router-dom';
+import './home.css';
 
 // URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
-import { Link } from 'react-router-dom';
 
 function Home(){
   const [filmes, setFilmes] = useState([]);
-
+  const [loading, setLoading] = useState(true);
 
 
   useEffect(()=>{
@@ -15,33 +15,45 @@ function Home(){
     async function loadFilmes(){
       const response = await api.get("movie/now_playing", {
         params:{
-         api_key: "d970c7b3db3005dd4701df9dd32cfcc1",
+         api_key: "28fc232cc001c31e8a031f419d0a14ca",
          language: "pt-BR",
          page: 1,
         }
       })
 
-      setFilmes(response.data.results.slice(0, 10));
-    
- }
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10))
+      setLoading(false);
+
+    }
 
     loadFilmes();
 
   }, [])
 
+
+
+  if(loading){
+    return(
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
+
   return(
-    <div className='container'>
-        <div className='lista-filmes'>
-            {filmes.map((filme) => {
-              return(
-                <article key={filme.id}>
-                    <h1 className='title'>{filme.title}</h1>
-                    <img className='poster' src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
-                    <Link className='link' to={`/filme/${filme.id}`}>Acessar</Link>
-                </article>
-              )
-            })}
-        </div>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Filme from './pages/Filme';
+import Favoritos from './pages/Favoritos'
+
+import Erro from './pages/Erro';
 
 import Header from './components/Header';
 
@@ -12,6 +15,9 @@ function RoutesApp(){
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/filme/:id" element={ <Filme/> } />
+        <Route path="/favoritos" element={ <Favoritos/> }/>
+
+        <Route path="*" element={ <Erro/> } />
       </Routes>
     </BrowserRouter>
   )
